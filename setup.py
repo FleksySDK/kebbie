e2891d41f0815eae0ac6817b31253acf1a1650bf
Astariul <remondnicola@gmail.com>
2024-03-27 10:42:41 +0900

    🔖 CHange version for release 0.1.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ extras_require["dev"] = (
 
 setuptools.setup(
     name="kebbie",
-    version="1.0.0.dev0",
+    version="0.1.1",
     author="Nicolas REMOND",
     author_email="nicolas.remond@thingthing.co",
     description="A small framework to test and compare mobile keyboards",
