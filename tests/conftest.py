ed07e91174f20b491ed73dc538f8b4073c607143
Astariul <remondnicola@gmail.com>
2024-05-09 18:57:11 +0900

    ✅ Add session-based monkey patch and forbid usage of requests in unit-tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,19 @@ import random
 import pytest
 
 
+@pytest.fixture(autouse=True)
+def no_requests(monkeypatch):
+    # Make sure unit-tests don't use any resources pulled from the internet
+    # using `requests`
+    monkeypatch.delattr("requests.sessions.Session.request")
+
+
+@pytest.fixture(scope="session")
+def monkeypatch_session():
+    with pytest.MonkeyPatch.context() as mp:
+        yield mp
+
+
 @pytest.fixture
 def seeded():
     random.seed(36)
