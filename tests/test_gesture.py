a818e40b8ec1339fdb35ebbfa4fdb67cfbcac4ca
Astariul <remondnicola@gmail.com>
2024-05-09 11:14:55 +0900

    ✅ Add unit-tests for swipe gesture generation

---
+++ tests/test_gesture.py
@@ -0,0 +1,37 @@
+import random
+import pytest
+
+from kebbie.gesture import make_swipe_gesture, MIN_N_POINTS_PER_DIST, MAX_N_POINTS_PER_DIST
+from kebbie.utils import euclidian_dist
+
+
+@pytest.fixture
+def seed():
+    random.seed(36)
+
+
+def test_make_swipe_gesture_between_2_points(seed):
+    control_points = [(0, 0), (100, 100)]
+
+    points = make_swipe_gesture(control_points)
+
+    d = euclidian_dist(control_points[0], control_points[1])
+    assert int(d * MIN_N_POINTS_PER_DIST) <= len(points) <= int(d * MAX_N_POINTS_PER_DIST)
+
+
+def test_make_swipe_gesture_between_more_points(seed):
+    points = make_swipe_gesture([(0, 0), (100, 100), (50, 80), (-100, 10)])
+    assert len(points) > 4
+
+
+def test_make_swipe_gesture_single_control_point():
+    assert make_swipe_gesture([(0, 0)]) == [(0, 0)]
+
+
+def test_make_swipe_gesture_same_points():
+    assert make_swipe_gesture([(0, 0), (0, 0)]) == [(0, 0)]
+
+
+def test_make_swipe_gesture_too_small_points(seed):
+    points = make_swipe_gesture([(0, 0), (0.1, 0)])
+    assert len(points) > 2
