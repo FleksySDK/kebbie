091641e594fa9f8969fb759475d2ea3b1b9b6ba2
David Calvo <david.calvo@thingthing.co>
2024-05-17 12:03:53 +0200

    fixed lint and badge coverage

--- kebbie/emulator.py
+++ kebbie/emulator.py
@@ -258,7 +258,7 @@ class Emulator:
         ValueError: Error raised if the given platform doesn't exist.
     """
 
-    def __init__(
+    def __init__(  # noqa: C901
         self,
         platform: str,
         keyboard: str,
@@ -267,7 +267,7 @@ class Emulator:
         port: str = "4723",
         ios_name: str = None,
         ios_platform: str = None,
-    ):  # noqa: C901
+    ):
         super().__init__()
 
         self.platform = platform.lower()
